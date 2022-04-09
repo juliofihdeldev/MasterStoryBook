e521de37420451d4e3e826725e7ce92377b43c25
Julio JEAN FILS <julio.fils1@gmail.com>
2022-04-09 14:38:59 -0400

    Create UI in story book

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
+  stories: ['../src/components/**/*.stories.js'], 
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
